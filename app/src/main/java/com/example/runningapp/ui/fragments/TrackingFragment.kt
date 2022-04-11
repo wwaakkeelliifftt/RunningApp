6254d364cf5c4b49314b56b0af743dae516554c3
wakelift <esazlesa@gmail.com>
2022-04-11 04:08:02 +0300

    GoogleMap: lifecycle binding for xml-type MapView object (doesn't need for same object - MapFragment)

--- app/src/main/java/com/example/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/TrackingFragment.kt
@@ -8,12 +8,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.runningapp.databinding.FragmentTrackingBinding
 import com.example.runningapp.ui.viewModels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment: Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
+    private var map: GoogleMap? = null
 
     private var _binding: FragmentTrackingBinding? = null
     private val binding get() = _binding!!
@@ -27,8 +29,56 @@ class TrackingFragment: Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mapView.apply {
+            onCreate(savedInstanceState)
+            getMapAsync {
+                map = it
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    /** probably @mapView already destroy at this moment and need null-check action ?? */
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        map = null
     }
 }
